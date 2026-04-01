bef364f689a82821f458c0e92f2c1864c93187ff
Softjit Singh <besofttech8@gmail.com>
2026-04-01 10:51:02 +0530

    errors

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = ['nativewind/babel', // Use plugin, not preset
-      // 'react-native-worklets/plugin',
-      'react-native-reanimated/plugin',];
-
-
-
+  
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-    plugins,
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }], 
+      'nativewind/babel'
+    ],
+    plugins: [
+      
+      'react-native-reanimated/plugin',
+    ],
   };
 };
